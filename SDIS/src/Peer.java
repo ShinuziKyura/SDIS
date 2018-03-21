b047536366618c74cc97b87dfe91b04b7666ffb4
pc <pc@172.30.29.75>
2018-03-21 11:30:02 +0000

    solved errors

--- SDIS/src/Peer.java
+++ SDIS/src/Peer.java
@@ -1,19 +1,20 @@
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.UUID;
 
 public class Peer {
 	private int id;
-	private MulticastConnection mc_socket;
-	private MulticastConnection mdb_socket;
-	private MulticastConnection mdr_socket;
+	private MulticastConnection mc_socket; //multicast control
+	private MulticastConnection mdb_socket; // multicast data backup
+	private MulticastConnection mdr_socket; // multicast data restore
 	
 	public static String DBS_TEST = "dbstest";
 	
-	public Peer(char[3] protocol_version, int server_id, String access_point, 
+	public Peer(char[] protocol_version, int server_id, String access_point, 
 				String mc_addr, short mc_port, 
 				String mdb_addr, short mdb_port, 
-				String mdr_addr, short mdr_port)
+				String mdr_addr, short mdr_port) throws IOException
 	{
 		id = server_id;
 		mc_socket = new MulticastConnection(mc_addr, mc_port);
