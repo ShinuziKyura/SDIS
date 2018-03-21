e0a7b39bf5e5d295a43607ab3b1bbe1e282af3b8
WORK PC <WORK PC@172.30.19.178>
2018-03-21 12:57:08 +0000

    Added PeerListener

--- SDIS/src/dbs/PeerListener.java
+++ SDIS/src/dbs/PeerListener.java
@@ -1,12 +1,37 @@
 package dbs;
 
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class PeerListener implements Runnable
 {
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
+	private MulticastConnection socket;
+	private AtomicBoolean running;
+	
+	public PeerListener(MulticastConnection socket)
+	{
+		this.socket = socket;
+		this.running = new AtomicBoolean(true);
 	}
 	
+	@Override
+	public void run() {
+		while (running.get())
+		{
+			try
+			{
+				String message = socket.receive();
+			}
+			catch (IOException e)
+			{
+				
+			}
+			
+			// Process message
+		}
+	}
+	
+	public void stop() {
+		this.running.set(false);
+	}
 }
